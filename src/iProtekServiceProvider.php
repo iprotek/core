14e42477561c38ab643b30e9b8552cf3ada2c9ac
drimcaster <drimcaster@gmail.com>
2024-06-02 04:50:12 +0800

    Update iProtekServiceProvider.php

--- src/iProtekServiceProvider.php
+++ src/iProtekServiceProvider.php
@@ -54,7 +54,7 @@ class iProtekServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'iprotek_core');
 
         
-        $this->app->singleton('Illuminate\Contracts\Http\Kernel', function ($app) {
+        $this->app->extend('Illuminate\Contracts\Http\Kernel', function ($app) {
             return new Kernel($app, $app['router']);
         });
     }
