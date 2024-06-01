b9050ba4768bdd64acbffbe3eec6774017d5c044
drimcaster <drimcaster@gmail.com>
2024-06-02 00:11:00 +0800

    Update iProtekServiceProvider.php

--- src/iProtekServiceProvider.php
+++ src/iProtekServiceProvider.php
@@ -24,6 +24,11 @@ class iProtekServiceProvider extends ServiceProvider
     public function boot()
     {
         // Bootstrap package services
+        
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+        
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'iprotek_core');
     }
