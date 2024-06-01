0db4c5e7a767fea5d513b36157305a836ec5cc13
drimcaster <drimcaster@gmail.com>
2024-06-02 05:26:47 +0800

    Update Kernel.php

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -13,6 +13,16 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+    protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
 
     /**
      * The application's route middleware groups.
@@ -20,6 +30,28 @@ class Kernel extends HttpKernel
      * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //\Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
+        ],
+        "customer"=>[
+            \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
+        ],
+        'api' => [            
+            \App\Http\Middleware\EncryptCookies::class,
+            //'throttle:60,1', 
+            \Illuminate\Session\Middleware\StartSession::class,
+            //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            //\App\Http\Middleware\EncryptCookies::class,
+        ], 
         "web-visits"=>[
             \iProtek\Core\Http\Middleware\TrackVisitor::class
         ]   
@@ -33,6 +65,18 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [ 
+        'auth.customer' => \App\Http\Middleware\AuthenticateCustomer::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'throttle.login' => \App\Http\Middleware\ThrottleLoginAttempts::class, 
+        
         'pay.account'=>\iProtek\Core\Http\Middleware\PayAppUserAccount::class,
         'pay.api'=>\iProtek\Core\Http\Middleware\PayAppUserAccountApi::class,
         'super_admin'=>\iProtek\Core\Http\Middleware\SuperAdmin::class,
