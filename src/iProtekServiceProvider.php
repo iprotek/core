00d270536cc49ca2e79659289c3cf4317a5adaa2
drimcaster <drimcaster@gmail.com>
2024-06-02 00:45:48 +0800

    location update

--- src/iProtekServiceProvider.php
+++ src/iProtekServiceProvider.php
@@ -29,6 +29,10 @@ class iProtekServiceProvider extends ServiceProvider
         //    __DIR__.'/../database/migrations' => database_path('migrations'),
         //], 'migrations');
 
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/iprotek'),
+        ], 'public');
+
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'iprotek_core');
