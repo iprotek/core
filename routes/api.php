4052dfb54c1a480451a2db00abaec94e3f0a7ec3
drimcaster <drimcaster@gmail.com>
2024-06-09 17:04:53 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,20 @@ use iProtek\Core\Http\Controllers\Manage\FileUploadController;
 Route::middleware('api')->group(function(){
 
 
-    Route::prefix('/file-upload')->name('.file-upload')->group(function(){
-    
-        Route::post('/add', [ FileUploadController::class ,'api_add'])->name('.add');
-        Route::get('/get-list', [ FileUploadController::class ,'list'])->name('.get-list');
-        Route::get('/get-list/{id}', [ FileUploadController::class ,'list'])->name('.get-one');
-        Route::get('/image-preview/{id}', [ FileUploadController::class ,'image_preview'])->name('.image-preview');
-        Route::get('/load-file/{id}', [ FileUploadController::class ,'load_file'])->name('.load-file');
-        Route::post('/set-default/{id}', [ FileUploadController::class ,'set_default'])->name('.set-default');
-        Route::delete('/remove/{id}', [ FileUploadController::class ,'remove'])->name('.remove');
-    
+    Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){
+
+        Route::prefix('/file-upload')->name('.file-upload')->group(function(){
+        
+            Route::post('/add', [ FileUploadController::class ,'api_add'])->name('.add');
+            Route::get('/get-list', [ FileUploadController::class ,'list'])->name('.get-list');
+            Route::get('/get-list/{id}', [ FileUploadController::class ,'list'])->name('.get-one');
+            Route::get('/image-preview/{id}', [ FileUploadController::class ,'image_preview'])->name('.image-preview');
+            Route::get('/load-file/{id}', [ FileUploadController::class ,'load_file'])->name('.load-file');
+            Route::post('/set-default/{id}', [ FileUploadController::class ,'set_default'])->name('.set-default');
+            Route::delete('/remove/{id}', [ FileUploadController::class ,'remove'])->name('.remove');
+        
+        });
+
     });
+    
 });
