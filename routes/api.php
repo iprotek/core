e0bb958ded8b5550e4e9adb1d886c55f62429920
drimcaster <37214264+drimcaster@users.noreply.github.com>
2024-06-10 17:25:09 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -7,25 +7,29 @@ use iProtek\Core\Http\Controllers\AppVariableController;
 //Route::prefix('sms-sender')->name('sms-sender')->group(function(){
   //  Route::get('/', [SmsController::class, 'index'])->name('.index');
 //});
-Route::middleware('api')->prefix('api')->group(function(){
+Route::middleware('api')->group(function(){
+    Route::prefixt('api')->group(function(){
 
-    Route::get('app-list', [AppVariableController::class, 'api_applist'])->name('api.app-list');
-    Route::get('raw-app-list', [AppVariableController::class, 'raw_api_applist'])->name('api.raw-app-list');
+        Route::get('app-list', [AppVariableController::class, 'api_applist'])->name('api.app-list');
+        Route::get('raw-app-list', [AppVariableController::class, 'raw_api_applist'])->name('api.raw-app-list');
 
-    Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){
+        Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){
 
-        Route::prefix('/file-upload')->name('.file-upload')->group(function(){
-        
-            Route::post('/add', [ FileUploadController::class ,'api_add'])->name('.add');
-            Route::get('/get-list', [ FileUploadController::class ,'list'])->name('.get-list');
-            Route::get('/get-list/{id}', [ FileUploadController::class ,'list'])->name('.get-one');
-            Route::get('/image-preview/{id}', [ FileUploadController::class ,'image_preview'])->name('.image-preview');
-            Route::get('/load-file/{id}', [ FileUploadController::class ,'load_file'])->name('.load-file');
-            Route::post('/set-default/{id}', [ FileUploadController::class ,'set_default'])->name('.set-default');
-            Route::delete('/remove/{id}', [ FileUploadController::class ,'remove'])->name('.remove');
-        
+            Route::prefix('/file-upload')->name('.file-upload')->group(function(){
+            
+                Route::post('/add', [ FileUploadController::class ,'api_add'])->name('.add');
+                Route::get('/get-list', [ FileUploadController::class ,'list'])->name('.get-list');
+                Route::get('/get-list/{id}', [ FileUploadController::class ,'list'])->name('.get-one');
+                Route::get('/image-preview/{id}', [ FileUploadController::class ,'image_preview'])->name('.image-preview');
+                Route::get('/load-file/{id}', [ FileUploadController::class ,'load_file'])->name('.load-file');
+                Route::post('/set-default/{id}', [ FileUploadController::class ,'set_default'])->name('.set-default');
+                Route::delete('/remove/{id}', [ FileUploadController::class ,'remove'])->name('.remove');
+            
+            });
+
         });
 
+        
     });
 
 });
