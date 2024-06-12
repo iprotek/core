6faac92567ff5f0dc99f6c749b316edd5864fe8a
drimcaster <drimcaster@gmail.com>
2024-06-12 19:27:46 +0800

    update

--- resources/js/components/manage/HeaderMenu.vue
+++ resources/js/components/manage/HeaderMenu.vue
@@ -1,26 +1,8 @@
-  <!-- Navbar -->
-  <nav id="top-menu" class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <!--
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="../index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>-->
-    </ul>
-
-    <!-- Right navbar links -->
+<template>
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
-      
       <li class="nav-item">
         <select class="form-control languagelist">
-        </select>
       </li>
       <li class="nav-item">
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
@@ -139,10 +121,29 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?=view()->getSection('logout-link','#')?>" role="button">
+        <a class="nav-link" href="/logout" role="button">
           <i class="far fa-user"></i>
         </a>
       </li>
     </ul>
-  </nav>
-  <!-- /.navbar -->
+</template>
+
+<script>
+    export default {
+        props:[  ],
+        components: { 
+        },
+        data: function () {
+            return {    
+            }
+        },
+        methods: { 
+
+        },
+        mounted:function(){     
+        },
+        updated:function(){
+
+        }
+    }
+</script>
