f4ccc23df1653c24bcb2ba6f167283a73af0dff5
drimcaster <drimcaster@gmail.com>
2024-06-22 21:42:13 +0800

    initial

---
+++ resources/js/components/manage/notification/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Updates</div> 
+                    <div class="card-body"> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[  ],
+        components: { 
+        },
+        data: function () {
+            return {    
+            }
+        },
+        methods: { 
+
+        },
+        mounted:function(){     
+        },
+        updated:function(){
+
+        }
+    }
+</script>
