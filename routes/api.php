270c2c34fc9caa61073b504f5314008d9f96b90f
drimcaster <drimcaster@gmail.com>
2024-06-23 11:05:16 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route; 
 use iProtek\Core\Http\Controllers\Manage\FileUploadController; 
-use iProtek\Core\Http\Controllers\AppVariableController; 
-use iProtek\Core\Http\Controllers\AppInfoController; 
+use iProtek\Core\Http\Controllers\AppVariableController;
 
 //Route::prefix('sms-sender')->name('sms-sender')->group(function(){
   //  Route::get('/', [SmsController::class, 'index'])->name('.index');
@@ -11,7 +10,7 @@ use iProtek\Core\Http\Controllers\AppInfoController;
 Route::middleware('api')->group(function(){
     Route::prefix('api')->group(function(){
 
-        Route::get('app-info', [AppInfoController::class, 'app_info'])->name('api.app-info');
+        Route::get('app-info', [\App\Http\Controllers\AppInfoController::class, 'app_info'])->name('api.app-info');
 
         Route::get('app-list', [AppVariableController::class, 'api_applist'])->name('api.app-list');
         Route::post('raw-app-list', [AppVariableController::class, 'raw_api_applist'])->name('api.raw-app-list');
