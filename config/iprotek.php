143a307552dbdeb7a030e7c8b35ccba0dfa9e6d8
drimcaster <drimcaster@gmail.com>
2024-06-23 10:56:35 +0800

    Update iprotek.php

--- config/iprotek.php
+++ config/iprotek.php
@@ -6,7 +6,7 @@ return [
     'pay_client_id'=>env('PAY_IPROTEK_CLIENT_ID', '0'),
     'pay_url'=>env('PAY_IPROTEK_URL', ''),
     'pay_client_secret'=>env('PAY_IPROTEK_CLIENT_SECRET', ''),
-    'api_version'=>env('APP_API_VERSION', '1.0.0.0'),
-    'api_db_version'=>env('APP_API_DB_VERSION', '1.0.0.0'),
+    'api_version'=>env('APP_API_VERSION', '1.0.0.1'),
+    'api_db_version'=>env('APP_API_DB_VERSION', '1.0.0.1'),
     'api_description'=>env('APP_DESCRIPTION', '')
 ];
