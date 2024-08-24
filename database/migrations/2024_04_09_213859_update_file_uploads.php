2c4f8017c5152e3301f116c6b1a4d67927c1604a
drimcaster <drimcaster@gmail.com>
2024-08-24 23:44:50 +0800

    Update 2024_04_09_213859_update_file_uploads.php

--- database/migrations/2024_04_09_213859_update_file_uploads.php
+++ database/migrations/2024_04_09_213859_update_file_uploads.php
@@ -15,6 +15,27 @@ class UpdateFileUploads extends Migration
     { 
         
         if(Schema::hasTable('file_uploads')) { 
+            
+            if (!Schema::hasColumn('file_uploads', 'group_id')) {
+                Schema::table('file_uploads', function (Blueprint $table) {
+                    $table->BigInteger('group_id')->nullable();
+                });
+            }
+            if (!Schema::hasColumn('file_uploads', 'pay_created_by')) {
+                Schema::table('file_uploads', function (Blueprint $table) {
+                    $table->BigInteger('pay_created_by')->nullable();
+                });
+            }
+            if (!Schema::hasColumn('file_uploads', 'pay_updated_by')) {
+                Schema::table('file_uploads', function (Blueprint $table) {
+                    $table->BigInteger('pay_updated_by')->nullable();
+                });
+            }
+            if (!Schema::hasColumn('file_uploads', 'pay_deleted_by')) {
+                Schema::table('file_uploads', function (Blueprint $table) {
+                    $table->BigInteger('pay_deleted_by')->nullable();
+                });
+            }
             return;
         } 
 
