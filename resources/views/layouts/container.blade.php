0986ab6337661e7fc8092ea250bc0db4ab9c8889
drimcaster <drimcaster@gmail.com>
2024-08-27 03:32:50 +0800

    Update container.blade.php

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -96,7 +96,7 @@
                                         {{ Auth::user()->name }}
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown" style="z-index:10000;">
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
