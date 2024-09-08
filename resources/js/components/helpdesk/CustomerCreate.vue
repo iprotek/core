6483f43a82f4d45fb561bd4a5f38cfebf74407c4
drimcaster <drimcaster@gmail.com>
2024-09-09 01:18:00 +0800

    update

---
+++ resources/js/components/helpdesk/CustomerCreate.vue
@@ -0,0 +1,37 @@
+<template>
+    <div >
+        <div class="rowmt-2">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">CREATE TICKET</div>
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <swal ref="swal_prompt"></swal>
+    </div>
+</template>
+
+<script>
+    import SwalVue from '../common/Swal.vue';
+    export default {
+        props:[  ],
+        components: { 
+            "swal":SwalVue
+        },
+        data: function () {
+            return {    
+            }
+        },
+        methods: { 
+
+        },
+        mounted:function(){     
+        },
+        updated:function(){
+
+        }
+    }
+</script>
