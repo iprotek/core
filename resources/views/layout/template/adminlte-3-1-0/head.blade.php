a34a9600a2cb5bb12f8b1ce86a1096872dfcf888
drimcaster <drimcaster@gmail.com>
2024-09-09 12:48:05 +0800

    Update head.blade.php

--- resources/views/layout/template/adminlte-3-1-0/head.blade.php
+++ resources/views/layout/template/adminlte-3-1-0/head.blade.php
@@ -26,7 +26,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/iprotek/design/templates/adminlte3.1.0/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
-    <!--<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">-->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet" href="/iprotek/design/templates/adminlte3.1.0/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
