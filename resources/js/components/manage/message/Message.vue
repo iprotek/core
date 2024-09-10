bf03fc47ca2b64b72ca9c61d16a12ca155d2cbdc
drimcaster <drimcaster@gmail.com>
2024-09-11 03:02:57 +0800

    update

--- resources/js/components/manage/message/Message.vue
+++ resources/js/components/manage/message/Message.vue
@@ -3,66 +3,40 @@
         <div>
             <a class="nav-link" data-toggle="dropdown" href="#" >
                 <i class="far fa-comments text-primary"></i>
-                <span v-if="has_chat" class="badge badge-warning navbar-badge">!</span>
+                <span v-if="!has_chat" class="badge badge-warning navbar-badge">!</span>
                 <span v-else class="badge badge-danger navbar-badge">3</span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <template v-if="has_chat">
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                        <img alt="User Avatar" class="img-size-50 mr-3 img-circle"/>
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                            Brad Diesel
-                            <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">Call me whenever you can...</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                    <div class="p-1" @click.stop="">
+                        <div class="input-group text-sm mb-1">
+                            <span class="btn btn-default">
+                                <small title="Show" class="fa fa-search text-primary"></small>
+                            </span> 
+                            <input type="text" class="form-control" placeholder="Search chats here..">
                         </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                        <img alt="User Avatar" class="img-size-50 img-circle mr-3"/>
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                            John Pierce
-                            <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">I got your message bro</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                        <img alt="User Avatar" class="img-size-50 img-circle mr-3"/>
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                            Nora Silvester
-                            <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">The subject goes here</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
+                    </div>
+                    <div class="dropdown-divider" @click.stop=""></div>
+                    <notification-container v-if="ai" :header="'Ask AI'" :data="ai"></notification-container>
+                    <notification-container v-if="group" :header="'Group Messages'" :data="group"></notification-container>
+                    <notification-container v-if="team" :header="'Team Messages'" :data="team"></notification-container>
+                    <notification-container v-if="dm" :header="'Direct Messages'" :data="dm"></notification-container>
                     <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
                 </template>
-                <template v-else>
-                    <a href="#" class="dropdown-item" onclick="window.addChatMessage({})">
+                <template v-else-if="isLoading">
+                    <a href="#" class="dropdown-item" >
                         <!-- Message Start -->
                         <div class="media">
-                            <code class="w-100 text-center"> -- Not Available at this moment -- </code>
+                            <code class="w-100 text-center"> -- Loading Chats -- </code>
+                        </div>
+                        <!-- Message End -->
+                    </a> 
+                </template>
+                <template v-else>
+                    <a href="#" class="dropdown-item">
+                        <!-- Message Start -->
+                        <div class="media">
+                            <code class="w-100 text-center" v-text="error_message">  </code>
                         </div>
                         <!-- Message End -->
                     </a> 
@@ -73,25 +47,54 @@
 </template>
 
 <script>
+    import NotificationContainerVue from './notification/NotificationContainer.vue';
     export default {
         props:[  ],
-        components: { 
+        components: {
+            "notification-container":NotificationContainerVue
         },
         data: function () {
             return {
-                has_chat:false
+                has_chat:false,
+                isLoading:false, 
+                error_message:'Chat Not Available',
+                dm:null,
+                ai:null,
+                group:null,
+                team:null,
             }
         },
         methods: { 
-            loadPushNotifSettings:function(){
+            clicktChatNotif:function(chatInfo){
+                window.addChatMessage({});
+            },
+            loadNotification:function(){
                 var vm = this;
-                WebRequest2('GET', '/manage/sms-sender/push-notif-info').then(resp=>{
+                vm.isLoading = true;
+                WebRequest2('GET', '/manage/message/notifications').then(resp=>{
+                    vm.isLoading = false;
                     resp.json().then(data=>{
-                        console.log("NOTIF SETTINGS", data);
-                        if(data.is_active && data.name == 'PUSHER.COM')
-                            vm.loadPusher();
+                        console.log("Info", data);
+                        if(data.status == 1){
+                            
+                            var result = data.result;
+                            vm.has_chat = true;
+                            vm.dm = result.dm;
+                            console.log("DM", result.dm);
+                            vm.ai = result.ai;
+                            vm.group = result.group;
+                            vm.team = result.team;
+ 
+
+                            
+                        }else{
+                            vm.has_chat = false;
+                            if(data.message)
+                                vm.error_message = data.message;
+                        }
                     });
                 });
+                //vm.loadPusherInfo();
             },
             loadPusher:function(details){
                 
@@ -110,10 +113,23 @@
                     console.log(data);
                     //app.messages.push(JSON.stringify(data));
                 });
+            },
+            loadPusherInfo:function(){
+
+                var vm = this;
+                /*
+                WebRequest2('GET', '/manage/message/push-notif-info').then(resp=>{
+                    resp.json().then(data=>{
+                        console.log("NOTIF SETTINGS", data);
+                        if(data.is_active && data.name == 'PUSHER.COM')
+                            vm.loadPusher();
+                    });
+                });
+                */
             }
         },
         mounted:function(){     
-            this.loadPushNotifSettings();
+            this.loadNotification();
             /*
             Pusher.logToConsole = true;
 
---
+++ resources/js/components/manage/message/notification/NotificationContainer.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div v-if="data">
+            <div class="media px-2" @click.stop="">
+                <div class="media-body">
+                    <b v-text="header"></b> 
+                    <small class="mx-1 badge badge-danger">3</small>
+                </div>
+            </div>
+            <div class="dropdown-divider mt-1"></div>
+            <div v-for="(chatItem, chatIndex) in users" v-bind:key="'chat-user-'+chatItem.id+'-'+_uid+'-'+chatIndex">
+                <a href="#"  class="dropdown-item" @click="clicktChatNotif(chatItem)"> 
+                    <div class="media">
+                        <img src="/images/temp-image.png" alt="User Avatar" class="img-size-50 mr-3 img-circle"/>
+                        <div class="media-body">
+                            <h3 class="dropdown-item-title">
+                            {{ chatItem.name }}
+                            <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                            </h3>
+                            <p class="text-sm">Call me whenever you can...</p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                        </div>
+                    </div> 
+                </a>
+                <div class="dropdown-divider"></div>
+            </div>
+            <div @click.stop="" class="text-center">
+                <div v-if="users.length == 0"  >
+                    <code> -- No Chat Available -- </code>
+                    <div class="dropdown-divider"></div>
+                </div>
+                <div v-else>
+                    <small class="text-center">
+                        <small> <i v-text="'and '+data.other_chat_count+' Others '"></i> </small>
+                    </small>
+                    <div class="dropdown-divider"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[ "header", "data" ],
+        components: { 
+        },
+        data: function () {
+            return { 
+                users:[]   
+            }
+        },
+        methods: { 
+            clicktChatNotif:function(chatInfo){
+                window.addChatMessage({});
+            },
+
+        },
+        mounted:function(){     
+            //console.log("DATA", this.data);
+            if(this.data && this.data.result){
+                this.users = this.data.result;
+            }
+        },
+        updated:function(){
+
+        }
+    }
+</script>
