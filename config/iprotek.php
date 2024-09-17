f85872d1dfdc5b8eb07c866c961dc038e2202674
drimcaster <drimcaster@gmail.com>
2024-09-17 15:54:40 +0800

    update

--- config/iprotek.php
+++ config/iprotek.php
@@ -9,5 +9,6 @@ return [
     'api_version'=>env('APP_API_VERSION', '1.0.0.1'),
     'api_db_version'=>env('APP_API_DB_VERSION', '1.0.0.1'),
     'api_description'=>env('APP_DESCRIPTION', ''),
-    'pay_message_url'=>env('PAY_MESSAGE_URL', '' )
+    'pay_message_url'=>env('PAY_MESSAGE_URL', '' ),
+    "pay_sms_report_url"=>env('PAY_SMS_REPORT_URL','www.iprotek.net')
 ];
