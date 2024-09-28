08307aceebf54b3305421b49bdccf71acdc52d77
drimcaster <drimcaster@gmail.com>
2024-09-28 11:48:44 +0800

    fix key

--- config/iprotek.php
+++ config/iprotek.php
@@ -12,5 +12,6 @@ return [
     'pay_message_url'=>env('PAY_MESSAGE_URL', '' ),
     "pay_sms_report_url"=>env('PAY_SMS_REPORT_URL','www.iprotek.net'),
     'app_type'=>env('PAY_IPROTEK_TYPE',''),
-    "cart_url"=>env('PAY_CART_URL', '')
+    "cart_url"=>env('PAY_CART_URL', ''),
+    "google_map_key"=>env('PAY_GOOGLE_MAP_KEY', '')
 ];
