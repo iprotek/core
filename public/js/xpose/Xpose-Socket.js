853c4efd88fe6ef2b1d8a85a36597dbf4c6c0e9d
drimcaster <drimcaster@gmail.com>
2024-12-24 23:06:21 +0800

    Update Xpose-Socket.js

--- public/js/xpose/Xpose-Socket.js
+++ public/js/xpose/Xpose-Socket.js
@@ -1,10 +1,11 @@
-window.XposeSocket = function(url, cluster, app_id, key){
+window.XposeSocket = async function(url, cluster, app_id, key){
     //var socket = new WebSocket(`${url}?cluster=${cluster}&app_id=${app_id}&key=${key}`);
-   return (async()=>{
+    
+    return ( async()=>{
         const socket = await window.openWebSocketWithTimeout(`${url}?cluster=${cluster}&app_id=${app_id}&key=${key}`, 3000);
-
-        if(!socket)
+        if(!socket){
             return;
+        }
 
         socket.settings = {
             cluster: cluster,
@@ -19,18 +20,33 @@ window.XposeSocket = function(url, cluster, app_id, key){
 
         };
 
-        socket.socket = function(app_id){
+        socket.status = function(app_id){
             return window.XposeSocketStatus(socket, app_id);
         }
+
+        socket.sendMessage = function(channel, event ,data, secret){
+            socket.send( JSON.stringify( {
+                secret: secret,
+                data: data,
+                event: event,
+                channel: channel,
+                type: 'message',
+                key: key
+            } )
+            );
+        } 
+
         return socket;
     })();
- 
     //return socket;
 }
 
 window.openWebSocketWithTimeout = function(url, timeout = 5000) {
     return new Promise((resolve, reject) => {
+
         const ws = new WebSocket(url);
+
+        window.XposeSocketSetMessage(ws);
 
         const timer = setTimeout(() => {
             ws.close(); // Close the WebSocket if not connected within the timeout
@@ -54,9 +70,22 @@ window.openWebSocketWithTimeout = function(url, timeout = 5000) {
 
 window.XposeSocketStatus = function(socket, app_id = ''){
     var res = {
-        then: null,
+        callback:null,
+        //This is trigger for then function
+        result_data:function(data){
+            if(!this.callback){
+                //console.log("Invalidate callback");
+                return;
+            }
+            this.callback(data);
+        },
+        //Triggering results for callbacks
+        then: function(fn){
+            this.callback = fn;
+        },
+        type:'status',
         app_id: app_id,
-        status_key: window.getRandomString(36)
+        status_key: window.XposegetRandomString(36)
     };
 
     if(!socket.callBacks)
@@ -65,13 +94,14 @@ window.XposeSocketStatus = function(socket, app_id = ''){
 
     
     //SET MESSAGE TRIGGER
-    window.XposeSocketSetMessage(socket);
+    //window.XposeSocketSetMessage(socket);
 
 
     //GET STATUS
     socket.send(JSON.stringify({
         type:'status',
-
+        status_key: res.status_key,
+        app_id: res.app_id
     }));
 
     return res;
@@ -80,9 +110,11 @@ window.XposeSocketStatus = function(socket, app_id = ''){
 window.XposeSocketSubscribe = function(socket, channel){
     var fn = {};
 
+    
     fn.listen = function(event){ 
         return window.XposeChannelEvent(socket, channel, event);
     };
+    
 
     return fn;
 }
@@ -90,24 +122,31 @@ window.XposeSocketSubscribe = function(socket, channel){
 window.XposeSocketSetMessage = function(socket){
     if(!socket.onmessage){
         socket.onmessage = function(evt){
+            
             const data = JSON.parse(evt.data);
+            if(!socket.callBacks){
+                return;
+            }
             socket.callBacks.forEach(item => {
                 
-                if(data.type == 'status'){
-                    if(data.status_key == item.status_key)
-                        item.then(data);
+                if(data.type == 'status' && data.type == item.type){
+                    if(data.status_key == item.status_key){
+                        item.result_data(data);
+                    }
                 }
-                if( data.type == 'subscribe' && data.channel == item.channel && item.event == data.event ){
+                else if( data.type == 'subscribe' && data.channel == item.channel && item.event == data.event ){
                     if(data.subscribe_key == item.subscribe_key){
-                        item.then(data);
-                        //console.log("subcribe info", data);
+                        item.result_data(data);
+                        console.log("Success to subscribe on channel:"+data.channel+' at event:'+data.event);
+                    }else{
+                        console.error("Failed to subscribe on channel:"+data.channel+' at event:'+data.event);
                     }
                 }
                 else if( data.channel == item.channel && item.event == data.event ){
-                    if(item.then){
-                        item.then(data);
+                    if(item.then){ 
+                        item.result_data(data);
                     }else{
-                        console.log("Unable to submit. Then empty.")
+                        console.log("Unable to submit 'then' is empty");
                     }
                 }
             });
@@ -119,18 +158,31 @@ window.XposeSocketSetMessage = function(socket){
 window.XposeChannelEvent = function(socket, channel, event){ 
 
     //STORE TO socket the function for executions
+
     var res = {
-        then: null,
+        callback:null,
+        //This is trigger for then function
+        result_data:function(data){
+            if(!this.callback){
+                //console.log("Invalidate callback");
+                return;
+            }
+            this.callback(data);
+        },
+        //Triggering results for callbacks
+        then: function(fn){
+            this.callback = fn;
+        },
         event: event,
         channel: channel,
-        subscribe_key: window.getRandomString(36)
+        subscribe_key: window.XposegetRandomString(36)
     };
     if(!socket.callBacks)
         socket.callBacks = [];
     socket.callBacks.push(res);
 
     //SET MESSAGE TRIGGER
-    window.XposeSocketSetMessage(socket);
+    //window.XposeSocketSetMessage(socket);
 
 
     //Subscribe HERE
@@ -139,15 +191,14 @@ window.XposeChannelEvent = function(socket, channel, event){
             subscribe_key: res.subscribe_key,
             channel: channel,
             event: event,
+            key: socket.settings.key,
             type: 'subscribe'
         }
     ) );
-
-
     return res;
 }
 
-window.getRandomString = function(length) {
+window.XposegetRandomString = function(length=36) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
