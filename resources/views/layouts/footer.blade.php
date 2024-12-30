5aa5aba22baa78dcc76b17a6d80db65690343115
drimcaster <drimcaster@gmail.com>
2024-12-30 17:09:18 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,10 @@
-    @include('iprotek_core::layouts.details-footer')    
-    @yield('foot')
+     
     <script src="/iprotek/design/templates/adminlte3.1.0/plugins/jquery-ui/jquery-ui.min.js"></script>
     <script src="/iprotek/design/templates/adminlte3.1.0/plugins/fullcalendar/main.js"></script> 
     <script src="/iprotek/design/templates/adminlte3.1.0/plugins/moment/moment.min.js"></script>
+
+    @include('iprotek_core::layouts.details-footer')
+    @yield('foot')
+    
 </body>
 </html>
