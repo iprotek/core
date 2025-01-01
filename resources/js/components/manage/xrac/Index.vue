2b0da947cbf3acf43c55aa0ed19b34111f3a56b8
drimcaster <drimcaster@gmail.com>
2025-01-02 02:39:51 +0800

    update

---
+++ resources/js/components/manage/xrac/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Extensible Role Access Control Settings</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[  ],
+        components: { 
+        },
+        data: function () {
+            return {    
+            }
+        },
+        methods: { 
+
+        },
+        mounted:function(){     
+        },
+        updated:function(){
+
+        }
+    }
+</script>
