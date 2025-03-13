345515c24a13c8771b38d4182d030153311e2ebf
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-03-13 15:07:19 +0800

    Update my-info.php

--- routes/manage/my-info.php
+++ routes/manage/my-info.php
@@ -6,7 +6,7 @@ use iProtek\Core\Http\Controllers\Manage\MyDetailController;
 
     //COMPANY DETAILS
     Route::prefix('/my-details')->name('.my-details')->group(function(){
-        Route::get('/',[MyDetailController::class, 'index']);
-        Route::get('/info',[MyDetailController::class, 'account_name_info']);
-        Route::put('/info',[MyDetailController::class, 'update_account_name_info']);
+        Route::get('/',[MyDetailController::class, 'index'])->name('.index');
+        Route::get('/info',[MyDetailController::class, 'account_name_info'])->name('.account-name-info');
+        Route::put('/info',[MyDetailController::class, 'update_account_name_info'])->name('.update-account-name-info');
     });  
