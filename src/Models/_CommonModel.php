69aa866ef66cd10eacc5d1790a3ff4da048e1440
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-03-30 10:17:21 +0800

    add other infos

--- src/Models/_CommonModel.php
+++ src/Models/_CommonModel.php
@@ -4,7 +4,8 @@ namespace iProtek\Core\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use iProtek\Core\Models\UserAdmin; 
+use iProtek\Core\Models\UserAdmin;
+use iProtek\Core\Models\UserAdminPayAccount;
 
 class _CommonModel extends Model
 {
@@ -21,6 +22,24 @@ class _CommonModel extends Model
 
     public function deleted_info(){
         return $this->belongsTo(UserAdmin::class, 'deleted_by')->select('id', 'name');
+    }
+
+    public function pay_created_info(){
+        return $this->belongsTo(UserAdminPayAccount::class, 'pay_created_by')->with(["user_admin"=>function($q){
+            $q->select('id', 'name');
+        }]);
+    }
+
+    public function pay_updated_info(){
+        return $this->belongsTo(UserAdminPayAccount::class, 'pay_updated_by')->with(["user_admin"=>function($q){
+            $q->select('id', 'name');
+        }]);
+    }
+
+    public function pay_deleted_info(){
+        return $this->belongsTo(UserAdminPayAccount::class, 'pay_deleted_by')->with(["user_admin"=>function($q){
+            $q->select('id', 'name');
+        }]);
     }
     
     public function file_uploads(){
