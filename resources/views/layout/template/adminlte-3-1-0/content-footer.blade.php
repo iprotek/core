00556e715eb76cd178ade7427410c2f9811e8e9e
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-04-11 12:06:25 +0800

    Update content-footer.blade.php

--- resources/views/layout/template/adminlte-3-1-0/content-footer.blade.php
+++ resources/views/layout/template/adminlte-3-1-0/content-footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright © 2021 <a href="#">iprotech-solutions.com</a>.</strong> All rights reserved.
+    <strong>Copyright © 2021 <a href="iprotek.net">iprotek.net</a>.</strong> All rights reserved.
   </footer>
