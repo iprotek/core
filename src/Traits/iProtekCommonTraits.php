d6096e133b093be292ece58656403a9aad1103bb
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-05-31 12:25:27 +0800

    Update iProtekCommonTraits.php

--- src/Traits/iProtekCommonTraits.php
+++ src/Traits/iProtekCommonTraits.php
@@ -11,29 +11,14 @@ trait iProtekCommonTraits
         "pay_deleted_by",
         "branch_id"
     ];
-    
-    protected static function booted()
-    {
-        /*
-        $commonFillable = static::$commonFillable;
-        static::creating(function ($model) use($commonFillable) {
-            $model->mergeFillable( $commonFillable );
-            //Log::error($model->fillable);
-        });
-        */
-    }
 
     public function initializeHasCommonAttributes()
     {
         // Merge with child model's $fillable (if defined)
-        //$this->mergeFillable($this->commonFillable);
-        
         $this->fillable = array_merge(
             $this->fillable ?? [],
             array_diff($this->commonFillable ?? [], $this->fillable ?? [])
         );
-        //if($this->getTable() == "sys_notify_schedulers")
-          //  Log::error($this->fillable);
     }
 
 }
