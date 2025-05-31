ff88700c2439355544c2e1bd682be5afdd4562f2
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-05-31 15:33:02 +0800

    update

---
+++ resources/js/components/manage/notification/scheduler/triggers/sms/IndexList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        SMS SCHEDULE TRIGGER LIST
+                    </div>
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[ "group_id", "branch_id" ],
+        $emits:[],
+        watch: { 
+        },
+        components: { 
+        },
+        data: function () {
+            return {    
+            }
+        },
+        methods: { 
+            queryString:function(params={}){ 
+                var queryString = Object.keys(params).map(function(key) {
+                    return key + '=' + params[key]
+                }).join('&');
+                return queryString;
+            },
+
+        },
+        mounted:function(){     
+        },
+        updated:function(){
+
+        }
+    }
+</script>
