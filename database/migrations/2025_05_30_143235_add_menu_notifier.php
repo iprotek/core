5a49b8877108ba301387fb0dd8307b4d34d6a21f
drimcaster <drimcaster@gmail.com>
2025-07-24 19:10:11 +0800

    Update 2025_05_30_143235_add_menu_notifier.php

--- database/migrations/2025_05_30_143235_add_menu_notifier.php
+++ database/migrations/2025_05_30_143235_add_menu_notifier.php
@@ -16,12 +16,19 @@ class AddMenuNotifier extends Migration
     {
         //  
         if(Schema::hasTable('accesses')) {
+        
             DB::table('accesses')->insert(
                 [ "id"=>19, "code"=>"manage-notify-scheduler", "name"=>"Notify Scheduler" ]
             ); 
+            
+            DB::select(" INSERT INTO `sys_sidemenu_items` (`id`,`ordinal`,`menu_text`,`url`,`icon`,`sidemenu_combo_id`,`sidemenu_group_id`,`control_access_id`,`created_at`,`updated_at`,`deleted_at`,`user_types`) VALUES (16,9,'Notify Scheduler','/manage/sys-notification/scheduler','fa-clock menu-sys-notification-scheduler',0,2,0,NULL,NULL,NULL,'0,1,2,3,4');");
+        
         }
-        DB::select(" INSERT INTO `sys_sidemenu_items` (`ordinal`,`menu_text`,`url`,`icon`,`sidemenu_combo_id`,`sidemenu_group_id`,`control_access_id`,`created_at`,`updated_at`,`deleted_at`,`user_types`) VALUES (9,'Notify Scheduler','/manage/sys-notification/scheduler','fa-clock menu-sys-notification-scheduler',0,2,0,NULL,NULL,NULL,'0,1,2,3,4');");
-   
+        else{
+
+            DB::select(" INSERT INTO `sys_sidemenu_items` (`ordinal`,`menu_text`,`url`,`icon`,`sidemenu_combo_id`,`sidemenu_group_id`,`control_access_id`,`created_at`,`updated_at`,`deleted_at`,`user_types`) VALUES (9,'Notify Scheduler','/manage/sys-notification/scheduler','fa-clock menu-sys-notification-scheduler',0,2,0,NULL,NULL,NULL,'0,1,2,3,4');");
+        
+        }
     }
 
     /**
