8ca620a2c67aa8c3ddf54d29cb53c60c90e60b6a
drimcaster <drimcaster@gmail.com>
2025-08-04 22:05:39 +0800

    Update Validation.vue

--- resources/js/components/common/Validation.vue
+++ resources/js/components/common/Validation.vue
@@ -1,11 +1,11 @@
 <template>
     <small v-if="is_small">
         <span v-if="errors[field]!==undefined" class="help-block text-red">
-            <span v-for="(value,key) in errors[field]" v-bind:key="'item-key'+key"> {{ value }}<br/></span>
+            <span v-for="(value,key) in errors[field]" v-bind:key="'item-key'+key" v-html="value"></span><br/>
         </span>
     </small>
     <span v-else-if="errors[field]!==undefined" class="help-block text-red">
-        <span v-for="(value,key) in errors[field]" v-bind:key="'item-key'+key"> {{ value }}<br/></span>
+        <span v-for="(value,key) in errors[field]" v-bind:key="'item-key'+key" v-html="value"> </span><br/>
     </span>
 </template>
 <script>
