945ba760fd4b21e6493217c05284e04565a34fed
drimcaster <drimcaster@gmail.com>
2025-08-08 22:48:17 +0800

    Update GoogleMap.vue

--- resources/js/components/common/GoogleMap.vue
+++ resources/js/components/common/GoogleMap.vue
@@ -5,7 +5,7 @@
 
 <script>
     export default {
-        props:[ "height", "width", "google_map_api_key" ],
+        props:[ "height", "width", "google_map_api_key", "coordinates", "is_multi_coordinates" ],
         $emits:[],
         watch: { 
         },
@@ -13,7 +13,9 @@
         },
         data: function () {
             return {
-                google_map_id: 'google-map-'+this._uid
+                google_map_id: 'google-map-'+this._uid,
+                map:null,
+                marker:null
             }
         },
         methods: { 
@@ -23,9 +25,38 @@
                 }).join('&');
                 return queryString;
             },
+            
+            initMap:function(coordinates=[{latitude: 10.3157, longitude: 123.8854}]) { // Default Cebu, Philippines
+
+                var vm = this;
+                const defaultLocation = { lat: coordinates[0].latitude, lng: coordinates[0].longitude }; 
+
+                vm.map = new google.maps.Map(document.getElementById(vm.map_identification), {
+                center: defaultLocation,
+                zoom: 10,
+                });
+
+                //vm.map.addListener("click", (e) => {
+                google.maps.event.addListenerOnce(vm.map, 'tilesloaded', function () {
+                    vm.placeMarker(defaultLocation);
+                });
+            },
+            placeMarker:function(location) {
+                var vm = this;
+                if (vm.marker) {
+                    vm.marker.setPosition(location);
+                } 
+                else {
+                    vm.marker = new google.maps.Marker({
+                        position: location,
+                        map: vm.map,
+                    });
+                }
+            }
 
         },
-        mounted:function(){     
+        mounted:function(){ 
+            this.initMap();
         },
         updated:function(){
 
