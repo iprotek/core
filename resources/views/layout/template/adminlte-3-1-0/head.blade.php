eba320061a0ee418b46f67268ed527a15a72258d
drimcaster <drimcaster@gmail.com>
2025-08-21 00:14:06 +0800

    Update head.blade.php

--- resources/views/layout/template/adminlte-3-1-0/head.blade.php
+++ resources/views/layout/template/adminlte-3-1-0/head.blade.php
@@ -77,8 +77,8 @@
         
         @keyframes bounce {
           0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
-          40% {transform: translateY(-10px);}
-          60% {transform: translateY(-5px);}
+          85% {transform: translateY(-10px);}
+          90% {transform: translateY(-5px);}
         }
         .bounce {
           display: inline-block;
