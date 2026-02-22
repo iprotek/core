c3371e9d1a6ccb40c9921a493c61929090792c1b
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-02-22 23:27:53 +0800

    Update Select2.vue

--- resources/js/components/common/Select2.vue
+++ resources/js/components/common/Select2.vue
@@ -141,7 +141,6 @@
                                 vm.custom_data(item_list);
                             }
 
-                            //console.log("GG", first_data_loaded, result, data);
                             if(!vm.first_data_loaded){
                                 vm.first_data_loaded = true;
                                 vm.$emit('first_data_load', result, data);
