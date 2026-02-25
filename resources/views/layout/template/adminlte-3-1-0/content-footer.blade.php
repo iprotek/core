47ae6df9b18d958ce0045b058a49957f9e50c35c
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-02-26 02:09:40 +0800

    update

--- resources/views/layout/template/adminlte-3-1-0/content-footer.blade.php
+++ resources/views/layout/template/adminlte-3-1-0/content-footer.blade.php
@@ -1,6 +1,6 @@
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
+      <b>Version</b> {{ config('assets.version') }}
     </div>
-    <strong>Copyright © 2021 <a href="iprotek.net">iprotek.net</a>.</strong> All rights reserved.
+    <strong>Copyright©{{date('Y')}} <a href="iprotek.net">iprotek.net</a>.</strong> All rights reserved.
   </footer>
